a6f06ace82fe9c062f602d09aec9cbe15f0dd37a
heo <ajdd06311@naver.com>
2026-05-28 18:39:23 +0900

    webSocket활용

---
+++ stock-project/backend/application/src/main/java/stock/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package stock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 💡 .withSockJS() 제거 (순수 웹소켓 사용)
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
