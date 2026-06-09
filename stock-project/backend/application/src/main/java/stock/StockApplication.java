ee7457846b23a72a37fe81d0e920664bb504bd5b
heo <ajdd06311@naver.com>
2026-06-09 15:48:19 +0900

    서울로 시간 기준

--- stock-project/backend/application/src/main/java/stock/StockApplication.java
+++ stock-project/backend/application/src/main/java/stock/StockApplication.java
@@ -1,12 +1,24 @@
 package stock;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class StockApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(StockApplication.class, args);
+
+
     }
+    //이제 무조건 서울기준으로 원래는 aws쓰느라 영국시간으로 되었어
+    @PostConstruct
+    public void started()
+    {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 }
